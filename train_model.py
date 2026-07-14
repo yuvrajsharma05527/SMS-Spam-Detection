ffccce2edaebe65cb7ac5fda97bc5e373e1320c8
Yuvraj <yuvrajsharma05527@gmail.com>
2026-07-14 20:21:54 +0530

    Fixed deployment issues and updated project

--- train_model.py
+++ train_model.py
@@ -16,66 +16,71 @@ nltk.download("punkt")
 nltk.download("punkt_tab")
 nltk.download("stopwords")
 
-# Load Dataset
+    # Load Dataset
 df = pd.read_csv("dataset/spam.csv", encoding="latin-1")
 
-# Remove unnecessary columns
+    # Remove unnecessary columns
 df = df[['v1', 'v2']]
 
-# Rename columns
+    # Rename columns
 df.columns = ['label', 'message']
-# Encode labels
+    # Encode labels
 encoder = LabelEncoder()
 df['label'] = encoder.fit_transform(df['label'])
+print("Classes:", encoder.classes_)
 
-# Apply preprocessing
+    # Apply preprocessing
 df['processed_message'] = df['message'].apply(transform_text)
 
-# Show first rows
+    # Show first rows
 print(df.head())
-
 print("\nShape of Dataset:")
 print(df.shape)
-
 print("\nSpam/Ham Count:")
 print(df['label'].value_counts())
-
 print("\nMissing Values:")
 print(df.isnull().sum())
-# TF-IDF Vectorization
+    # TF-IDF Vectorization
 tfidf = TfidfVectorizer(max_features=3000)
-
 X = tfidf.fit_transform(df['processed_message']).toarray()
 y = df['label']
-
 print("TF-IDF Shape:", X.shape)
 print("Labels Shape:", y.shape)
-
 # Split Dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
     test_size=0.2,
-    random_state=42
+    random_state=42,
+    stratify=y  
 )
-
 print("Training Data:", X_train.shape)
 print("Testing Data:", X_test.shape)
-
 # Train Model
 model = MultinomialNB()
-
 model.fit(X_train, y_train)
-
 # Prediction
 y_pred = model.predict(X_test)
-
 # Accuracy
 print("\nAccuracy :", accuracy_score(y_test, y_pred))
 print("Precision :", precision_score(y_test, y_pred))
-
 # Save Model
 joblib.dump(model, "model/spam_model.pkl")
 joblib.dump(tfidf, "model/vectorizer.pkl")
-
 print("\nModel Saved Successfully!")
+
+# Manual Testing
+test_messages = [
+    "Hey bro, where are you?",
+    "Congratulations! You have won a free iPhone. Claim now.",
+    "URGENT! You have won ₹50000 cash. Click here.",
+    "Let's meet at 6 PM."
+]
+
+for msg in test_messages:
+    processed = transform_text(msg)
+    vector = tfidf.transform([processed])
+    pred = model.predict(vector)[0]
+
+    print(f"\nMessage: {msg}")
+    print("Prediction:", "Spam" if pred == 1 else "Ham")
